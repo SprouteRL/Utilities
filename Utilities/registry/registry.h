11593cae909906acdf106366f4ecb4746679ea0a
Sproute <153927002+SprouteRL@users.noreply.github.com>
2024-12-23 15:25:20 +0200

    update

--- Utilities/registry/registry.h
+++ Utilities/registry/registry.h
@@ -1,8 +1,5 @@
-#pragma once
-
 #include <windows.h>
 #include <string>
-#include <vector>
 #include <stdexcept>
 #include <type_traits>
 
@@ -19,7 +16,7 @@ private:
 
 class Registry {
 public:
-    // Helper function to map registry value types to C++ types
+    // Helper function to map C++ types to registry value types
     template<typename T>
     struct RegistryTypeMapper;
 
@@ -33,11 +30,11 @@ public:
         static constexpr DWORD Type = REG_SZ;
     };
 
-    // Static templated function to read registry values
+    // Static templated function to write registry values
     template<typename T>
-    static T ReadRegistryValue(const std::string& path, const std::string& valueName) {
+    static void WriteRegistryValue(const std::string& path, const std::string& valueName, const T& valueData) {
         static_assert(std::is_same_v<T, DWORD> || std::is_same_v<T, std::string>,
-            "Unsupported type. Specialize RegistryTypeMapper for this type.");
+                      "Unsupported type. Specialize RegistryTypeMapper for this type.");
 
         // Determine the root key and subkey
         HKEY hKeyRoot;
@@ -46,109 +43,36 @@ public:
         if (path.rfind("HKEY_CLASSES_ROOT\\", 0) == 0) {
             hKeyRoot = HKEY_CLASSES_ROOT;
             subKey = path.substr(18);
-        }
-        else if (path.rfind("HKEY_CURRENT_USER\\", 0) == 0) {
+        } else if (path.rfind("HKEY_CURRENT_USER\\", 0) == 0) {
             hKeyRoot = HKEY_CURRENT_USER;
             subKey = path.substr(18);
-        }
-        else if (path.rfind("HKEY_LOCAL_MACHINE\\", 0) == 0) {
+        } else if (path.rfind("HKEY_LOCAL_MACHINE\\", 0) == 0) {
             hKeyRoot = HKEY_LOCAL_MACHINE;
             subKey = path.substr(19);
-        }
-        else if (path.rfind("HKEY_USERS\\", 0) == 0) {
+        } else if (path.rfind("HKEY_USERS\\", 0) == 0) {
             hKeyRoot = HKEY_USERS;
             subKey = path.substr(11);
-        }
-        else {
-            throw RegistryException("Invalid registry root key.", ERROR_INVALID_PARAMETER);
-        }
-
-        // Open the specified registry key
-        HKEY hKey;
-        LONG result = RegOpenKeyExA(hKeyRoot, subKey.c_str(), 0, KEY_QUERY_VALUE, &hKey);
-        if (result != ERROR_SUCCESS) {
-            throw RegistryException("Error opening registry key.", result);
-        }
-
-        // Query the size and type of the registry value
-        DWORD dataType = 0;
-        DWORD dataSize = 0;
-        result = RegQueryValueExA(hKey, valueName.c_str(), nullptr, &dataType, nullptr, &dataSize);
-        if (result != ERROR_SUCCESS) {
-            RegCloseKey(hKey);
-            throw RegistryException("Error querying registry value size.", result);
-        }
-
-        // Check if the data type matches the expected type
-        if (dataType != RegistryTypeMapper<T>::Type) {
-            RegCloseKey(hKey);
-            throw RegistryException("Registry value type mismatch.", ERROR_INVALID_DATATYPE);
-        }
-
-        // Allocate buffer and retrieve the actual data
-        T value;
-        if constexpr (std::is_same_v<T, DWORD>) {
-            if (dataSize != sizeof(DWORD)) {
-                RegCloseKey(hKey);
-                throw RegistryException("Unexpected DWORD size.", ERROR_INVALID_DATA);
-            }
-            result = RegQueryValueExA(hKey, valueName.c_str(), nullptr, nullptr,
-                reinterpret_cast<LPBYTE>(&value), &dataSize);
-        }
-        else if constexpr (std::is_same_v<T, std::string>) {
-            std::vector<char> buffer(dataSize);
-            result = RegQueryValueExA(hKey, valueName.c_str(), nullptr, nullptr,
-                reinterpret_cast<LPBYTE>(buffer.data()), &dataSize);
-            value.assign(buffer.data(), dataSize - 1); // Exclude null terminator
-        }
-
-        // Close the registry key
-        RegCloseKey(hKey);
-
-        if (result != ERROR_SUCCESS) {
-            throw RegistryException("Error reading registry value.", result);
-        }
-
-        return value;
-    }
-
-    // Static function to modify a DWORD registry value
-    static void ModifyRegistryValue(const std::string& path, const std::string& valueName, DWORD valueData) {
-        // Determine the root key and subkey
-        HKEY hKeyRoot;
-        std::string subKey;
-
-        if (path.rfind("HKEY_CLASSES_ROOT\\", 0) == 0) {
-            hKeyRoot = HKEY_CLASSES_ROOT;
-            subKey = path.substr(18);
-        }
-        else if (path.rfind("HKEY_CURRENT_USER\\", 0) == 0) {
-            hKeyRoot = HKEY_CURRENT_USER;
-            subKey = path.substr(18);
-        }
-        else if (path.rfind("HKEY_LOCAL_MACHINE\\", 0) == 0) {
-            hKeyRoot = HKEY_LOCAL_MACHINE;
-            subKey = path.substr(19);
-        }
-        else if (path.rfind("HKEY_USERS\\", 0) == 0) {
-            hKeyRoot = HKEY_USERS;
-            subKey = path.substr(11);
-        }
-        else {
+        } else {
             throw RegistryException("Invalid registry root key.", ERROR_INVALID_PARAMETER);
         }
 
         // Open or create the specified registry key
         HKEY hKey;
         LONG result = RegCreateKeyExA(hKeyRoot, subKey.c_str(), 0, nullptr, 0,
-            KEY_SET_VALUE, nullptr, &hKey, nullptr);
+                                      KEY_SET_VALUE, nullptr, &hKey, nullptr);
         if (result != ERROR_SUCCESS) {
             throw RegistryException("Error opening or creating registry key.", result);
         }
 
         // Set the registry value
-        result = RegSetValueExA(hKey, valueName.c_str(), 0, REG_DWORD,
-            reinterpret_cast<const BYTE*>(&valueData), sizeof(DWORD));
+        if constexpr (std::is_same_v<T, DWORD>) {
+            result = RegSetValueExA(hKey, valueName.c_str(), 0, RegistryTypeMapper<DWORD>::Type,
+                                    reinterpret_cast<const BYTE*>(&valueData), sizeof(DWORD));
+        } else if constexpr (std::is_same_v<T, std::string>) {
+            result = RegSetValueExA(hKey, valueName.c_str(), 0, RegistryTypeMapper<std::string>::Type,
+                                    reinterpret_cast<const BYTE*>(valueData.c_str()), static_cast<DWORD>(valueData.size() + 1));
+        }
+
         if (result != ERROR_SUCCESS) {
             RegCloseKey(hKey);
             throw RegistryException("Error setting registry value.", result);
