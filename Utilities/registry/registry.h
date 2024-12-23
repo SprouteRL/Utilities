676c8699d117766694198a3e372c2c43d401ecba
Sproute <153927002+SprouteRL@users.noreply.github.com>
2024-12-23 15:27:54 +0200

    Update registry.h

--- Utilities/registry/registry.h
+++ Utilities/registry/registry.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <windows.h>
 #include <string>
 #include <stdexcept>
@@ -35,7 +37,7 @@ public:
     template<typename T>
     static T ReadRegistryValue(const std::string& path, const std::string& valueName) {
         static_assert(std::is_same_v<T, DWORD> || std::is_same_v<T, std::string>,
-                      "Unsupported type. Specialize RegistryTypeMapper for this type.");
+            "Unsupported type. Specialize RegistryTypeMapper for this type.");
 
         HKEY hKeyRoot;
         std::string subKey;
@@ -54,13 +56,14 @@ public:
         if constexpr (std::is_same_v<T, DWORD>) {
             dataSize = sizeof(DWORD);
             result = RegQueryValueExA(hKey, valueName.c_str(), nullptr, &dataType,
-                                      reinterpret_cast<LPBYTE>(&valueData), &dataSize);
-        } else if constexpr (std::is_same_v<T, std::string>) {
+                reinterpret_cast<LPBYTE>(&valueData), &dataSize);
+        }
+        else if constexpr (std::is_same_v<T, std::string>) {
             result = RegQueryValueExA(hKey, valueName.c_str(), nullptr, &dataType, nullptr, &dataSize);
             if (result == ERROR_SUCCESS) {
                 std::vector<char> buffer(dataSize);
                 result = RegQueryValueExA(hKey, valueName.c_str(), nullptr, &dataType,
-                                          reinterpret_cast<LPBYTE>(buffer.data()), &dataSize);
+                    reinterpret_cast<LPBYTE>(buffer.data()), &dataSize);
                 valueData.assign(buffer.begin(), buffer.end() - 1); // Exclude null terminator
             }
         }
@@ -82,7 +85,7 @@ public:
     template<typename T>
     static void WriteRegistryValue(const std::string& path, const std::string& valueName, const T& valueData) {
         static_assert(std::is_same_v<T, DWORD> || std::is_same_v<T, std::string>,
-                      "Unsupported type. Specialize RegistryTypeMapper for this type.");
+            "Unsupported type. Specialize RegistryTypeMapper for this type.");
 
         HKEY hKeyRoot;
         std::string subKey;
@@ -90,18 +93,19 @@ public:
 
         HKEY hKey;
         LONG result = RegCreateKeyExA(hKeyRoot, subKey.c_str(), 0, nullptr, 0,
-                                      KEY_SET_VALUE, nullptr, &hKey, nullptr);
+            KEY_SET_VALUE, nullptr, &hKey, nullptr);
         if (result != ERROR_SUCCESS) {
             throw RegistryException("Error opening or creating registry key.", result);
         }
 
         if constexpr (std::is_same_v<T, DWORD>) {
             result = RegSetValueExA(hKey, valueName.c_str(), 0, RegistryTypeMapper<DWORD>::Type,
-                                    reinterpret_cast<const BYTE*>(&valueData), sizeof(DWORD));
-        } else if constexpr (std::is_same_v<T, std::string>) {
+                reinterpret_cast<const BYTE*>(&valueData), sizeof(DWORD));
+        }
+        else if constexpr (std::is_same_v<T, std::string>) {
             result = RegSetValueExA(hKey, valueName.c_str(), 0, RegistryTypeMapper<std::string>::Type,
-                                    reinterpret_cast<const BYTE*>(valueData.c_str()),
-                                    static_cast<DWORD>(valueData.size() + 1));
+                reinterpret_cast<const BYTE*>(valueData.c_str()),
+                static_cast<DWORD>(valueData.size() + 1));
         }
 
         RegCloseKey(hKey);
@@ -117,16 +121,20 @@ private:
         if (path.rfind("HKEY_CLASSES_ROOT\\", 0) == 0) {
             hKeyRoot = HKEY_CLASSES_ROOT;
             subKey = path.substr(18);
-        } else if (path.rfind("HKEY_CURRENT_USER\\", 0) == 0) {
+        }
+        else if (path.rfind("HKEY_CURRENT_USER\\", 0) == 0) {
             hKeyRoot = HKEY_CURRENT_USER;
             subKey = path.substr(18);
-        } else if (path.rfind("HKEY_LOCAL_MACHINE\\", 0) == 0) {
+        }
+        else if (path.rfind("HKEY_LOCAL_MACHINE\\", 0) == 0) {
             hKeyRoot = HKEY_LOCAL_MACHINE;
             subKey = path.substr(19);
-        } else if (path.rfind("HKEY_USERS\\", 0) == 0) {
+        }
+        else if (path.rfind("HKEY_USERS\\", 0) == 0) {
             hKeyRoot = HKEY_USERS;
             subKey = path.substr(11);
-        } else {
+        }
+        else {
             throw RegistryException("Invalid registry root key.", ERROR_INVALID_PARAMETER);
         }
     }
